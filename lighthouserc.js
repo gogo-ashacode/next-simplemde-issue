8785f608d8fa677eab15ed9cd273de84c561cd6f
Ash Aylsworth <102646559+gogo-ashacode@users.noreply.github.com>
2023-02-08 06:03:53 -0800

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,7 @@
 module.exports = {
   ci: {
-    collect: {
-      url: ["http://www.diarydirectory.com/"],
-    },
     upload: {
-      target: "temporary-public-storage",
+      target: 'temporary-public-storage',
     },
   },
 };
