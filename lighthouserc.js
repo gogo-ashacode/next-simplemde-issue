33ac6b35c411c9a0cea1c634ed8fa44f9fb53859
Ash Aylsworth <102646559+gogo-ashacode@users.noreply.github.com>
2023-02-08 06:21:11 -0800

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: ['http://localhost:3000/'],
-      startServerCommand: 'yarn dev',
+      startServerCommand: 'sudo yarn dev',
     },
     upload: {
       target: 'temporary-public-storage',
